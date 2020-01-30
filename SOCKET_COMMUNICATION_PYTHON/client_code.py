2107c5178f3e1d6668c9c6b4bf343fe2320d1ae7
jorgepdsML <45744833+jorgepdsML@users.noreply.github.com>
2020-01-29 21:31:39 -0500

    Update client_code.py

--- SOCKET_COMMUNICATION_PYTHON/client_code.py
+++ SOCKET_COMMUNICATION_PYTHON/client_code.py
@@ -5,6 +5,10 @@ Los datos que se enviaran al servidor se convertiran a un blujo de bytes (STREAM
 el uso del modulo pickle ,este paso es llamado Serialización . 
 Los datos que se enviaran deben estar en formato de BYTES 
 La serialización se realizara mediante la función dumps del modulo pickle
+--------------la clase socket de modulo socket recibe los argumentos siguientes para poder 
+establecer una comunición utilizando TCP y IP V4
+
+
 """
 import socket
 import numpy as np
@@ -14,10 +18,12 @@ import time
 HOST = '127.0.0.1'  # hostname del servidor o dirección IP
 PORT = 65432        # puerto de comunicación
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #CONECTARSE AL SERVIDOR CON HOST Y PORT DETERMINADOS DE ANTEMANO
+    #EL Mmetodo connect puede dar un error si en caso se sobrepasa el limite de tiempo
+    #para la conexión o tambien si el servidor no esta conectado a la red
     s.connect((HOST, PORT))
     print("CONECTADO AL SERVIDOR")
-    print("5 SEGUNDOS ")
-    time.sleep(5)
+    
     #mensaje a enviar
     img="PYTHON SOCKET CLIENTE"
     # serializar de objetos de python a flujo de bytes
@@ -26,12 +32,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     time.sleep(1)
     # enviar todo los bytes al servidor
     s.sendall(datos)
-    time.sleep(4)
-    s.sendall(datos+bytes("120","utf-8"))
-    print("CLIENTE : 10 SEGUNDOS")
-    time.sleep(10)
+    datos=pickle.dumps("ENVIANDO UN MENSAJE AL SERVIDOR ")
+    time.sleep(2)
+    s.sendall(datos)
+    print("ENVIANDO OTRO MENSAJE")
+    time.sleep(2)
     #Enviar ultimo mensaje
-    s.sendall(pickle.dumps("EL CLIENTE HA CADUCADO"))
+    s.sendall(pickle.dumps("MI ULTIMO MENSAJE AL SERVIDOR"))
 
 #codigo de fin de programa
 print("FIN PROGRAMA DEL  CLIENTE")
